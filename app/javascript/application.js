134778322b1ba8605a5297b8520d6080f289973c
Le Thi Trang <le.thi.trang.intern@vareal.vn>
2025-05-15 15:52:19 +0700

    add feature login

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from "bootstrap"
+import "bootstrap"
+
+import "@fortawesome/fontawesome-free"
+import "bootstrap/dist/css/bootstrap.min.css";import * as bootstrap from "bootstrap"
