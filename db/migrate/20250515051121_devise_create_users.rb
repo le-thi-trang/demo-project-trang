391c0a184dee36f7b7e3fad2a4590c6ac2c62c3d
Le Thi Trang <le.thi.trang.intern@vareal.vn>
2025-05-16 15:16:24 +0700

    feature login

--- db/migrate/20250515051121_devise_create_users.rb
+++ db/migrate/20250515051121_devise_create_users.rb
@@ -4,10 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ''
+      t.string :email, null: false, default: ''
       t.string :encrypted_password, null: false, default: ''
       t.string :phone_number, null: false, default: ''
-
+      t.string :name, null: false
+      t.date :date_of_birth, null: false
+      t.integer :position, null: false
+      t.string :information
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -36,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :phone_number, unique: true
     # add_index :users, :confirmation_token,   unique: true
