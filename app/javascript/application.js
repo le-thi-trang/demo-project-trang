b8cceb49b94a0c6473dbbf98721e9d4f0508eb9d
Le Thi Trang <le.thi.trang.intern@vareal.vn>
2025-05-19 17:02:38 +0700

    feature logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
-import "bootstrap"
+// import * as bootstrap from "bootstrap"
+// import "bootstrap"
 
 import "@fortawesome/fontawesome-free"
-import "bootstrap/dist/css/bootstrap.min.css";import * as bootstrap from "bootstrap"
+// import "@fortawesome/fontawesome-free/css/all.min.css"
+
+// import "bootstrap/dist/css/bootstrap.min.css";import * as bootstrap from "bootstrap"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,9 @@
 # Pin npm packages by running ./bin/importmap
+pin '@hotwired/turbo-rails', to: '@hotwired--turbo-rails.js' # @2.1.0
+pin '@hotwired/stimulus', to: 'stimulus.min.js'
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+# pin 'bootstrap', to: 'bootstrap.bundle.min.js'
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin 'application'
+# pin '@fortawesome/fontawesome-free', to: 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.7.2/css/all.min.css'
