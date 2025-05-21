b171bb26bad4179588015d409dc9c7372e3ec614
Le Thi Trang <le.thi.trang.intern@vareal.vn>
2025-05-21 15:16:20 +0700

    feature delete project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -36,6 +36,8 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path, notice: 'Project was successfully destroyed.'
   end
 
   private
